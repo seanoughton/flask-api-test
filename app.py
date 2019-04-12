86d6e8a174051eb4160d57435ba7d9b37a30b8a7
seanoughton <seanoughton@gmail.com>
2019-04-12 16:42:08 -0700

    created first route

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-import Flask
+from flask import Flask,render_template, request
 from flask-restful import Api, Resource
 
+app = Flask(__name__)
 
-@app.route('/'):
+
+@app.route('/', methods=['GET','POST']):
     def index():
         return "something"
 
