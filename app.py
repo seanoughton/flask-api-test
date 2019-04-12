3e9d751151da24c38ec324c6543f6b2aab18b5d7
seanoughton <seanoughton@gmail.com>
2019-04-12 16:56:53 -0700

    created basic todo app returning api info

--- app.py
+++ app.py
@@ -4,11 +4,19 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
+todos = {
+"1":"todo test 1"
+}
 
-api.add_resource(HelloWorld, '/')
+class TodoSimple(Resource):
+    def get(self, todo_id):
+        return {todo_id: todos[todo_id]}
+
+    def put(self, todo_id):
+        todos[todo_id] = request.form['data']
+        return {todo_id: todos[todo_id]}
+
+api.add_resource(TodoSimple, '/<string:todo_id>')
 
 
 
